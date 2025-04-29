5825f5d28b2cd0138e47fdc0a3dd214919e042a9
Natalie Yip <yip.natalie92@gmail.com>
2025-04-29 14:11:47 -0700

    Add images and new form component

---
+++ src/app/shorten-form/shorten-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shorten-form',
+  imports: [],
+  templateUrl: './shorten-form.component.html',
+  styleUrl: './shorten-form.component.scss'
+})
+export class ShortenFormComponent {
+
+}
