629ccccfa0499394de997f421374234261cd216a
Natalie Yip <yip.natalie92@gmail.com>
2025-04-29 14:55:36 -0700

    Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShortenFormComponent } from './shorten-form/shorten-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShortenFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shorten-form/shorten-form.component.ts
+++ src/app/shorten-form/shorten-form.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-shorten-form',
-  imports: [],
+  selector: 'shorten-form',
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './shorten-form.component.html',
   styleUrl: './shorten-form.component.scss'
 })
 export class ShortenFormComponent {
+
+  shortenForm: FormGroup; 
+
+  constructor(private fb: FormBuilder) {
+    this.shortenForm = this.fb.group({
+      url: ['', [Validators.required]]
+    });
+  }
+
+  submitForm() {
+  }
 
 }
